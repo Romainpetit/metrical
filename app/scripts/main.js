ad92d3b24884b32863c77670335d426a4de4605e
Romain Petit <contact@romainpetit.com>
2017-04-11 11:07:41 +0200

    Settings panel : make the checkboxes toggle classes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,16 +2,33 @@ console.log('\'Allo \'Allo!');
 
 document.addEventListener('keydown', function(event) {
     if (event.ctrlKey) {
-        // open helgp widget
           var layout = document.getElementById('layout')
           if (layout.classList.contains('off')) {
-            // The box that we clicked has a class of bad so let's remove it and add the good class
            layout.classList.remove('off');
            layout.classList.add('on');
           } else {
-            // The user obviously can't follow instructions so let's alert them of what is supposed to happen next
             layout.classList.remove('on');
             layout.classList.add('off');
           }
     }
 });
+
+document.getElementById("showElementBackground").addEventListener("click", function( event ) {
+  var body = document.body;
+
+  if (layout.classList.contains('ghost')) {
+   layout.classList.remove('ghost');
+  } else {
+    layout.classList.add('ghost');
+  }
+}, false);
+
+document.getElementById("showBaselineGrid").addEventListener("click", function( event ) {
+  var body = document.body;
+
+  if (layout.classList.contains('grid')) {
+   layout.classList.remove('grid');
+  } else {
+    layout.classList.add('grid');
+  }
+}, false);
